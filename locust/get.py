f5ef4f4034485abd559f1876d23b824184b60ab5
John Hardy <john.hardy@me.com>
2023-04-06 13:35:48 -0700

    * add locust load test

---
+++ locust/get.py
@@ -0,0 +1,28 @@
+from locust import HttpUser, task, between
+import random
+
+domains_frag = "ssl-{}.domain.{}"
+domain_suffix = [
+    "com",
+    "net",
+    "org",
+    "dev"
+]
+
+class Basic(HttpUser):
+    wait_time = between(0.5, 0.5)
+
+    def domain(self):
+        # assemble domain
+        suffix_key = random.randint(0, (len(domain_suffix) - 1))
+        suffix = domain_suffix[suffix_key]
+        site_key = random.randint(1, 10)
+        domain = domains_frag.format(site_key, suffix)
+
+        return domain
+        
+
+    @task
+    def cpu(self):
+        domain = self.domain()
+        self.client.get("https://{}".format(domain), verify=False)
